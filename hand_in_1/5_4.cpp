49d5b5014a1bb4ca5a9cd912b6534b5c1c063a09
Christian Holberg <holbergchris@hotmail.com>
2019-04-28 21:56:58 +0200

    Exercise 5.4: Initial and final commit.

---
+++ hand_in_1/5_4.cpp
@@ -0,0 +1,30 @@
+#include "5_4.h"
+#include <cassert>
+#include <cmath>
+
+double calc_std(double a[], int length)
+{
+	assert(length > 0);
+	double std = 0;
+	double x_bar = calc_mean(a, length);
+	if (length > 1)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			std += pow((a[i]-x_bar), 2);
+		}
+		std /= ((double)(length-1));
+	}
+	return std;
+}
+
+double calc_mean(double a[], int length)
+{
+	assert (length > 0);
+	double x_bar = 0;
+	for (int i = 0; i < length; i++)
+	{
+		x_bar += a[i];
+	}	
+	return x_bar / ((double)(length));
+}

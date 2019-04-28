374aefb3b99331a0cee7a67816d59094bb9f4359
Christian Holberg <holbergchris@hotmail.com>
2019-04-28 17:52:10 +0200

    Exercise 3_3: Final version with function. Changed y array to two different doubles. Changed main to void function.

--- hand_in_1/3_3.cpp
+++ hand_in_1/3_3.cpp
@@ -1,27 +1,23 @@
 #include "3_3.h"
 #include <cassert>
-#include <cmath>
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char* argv[])
+void implicit_Euler(int n)
 {
-	int n = atoi(argv[1]);
 	assert(n > 1);
 	double h = 1.0 / ((double)(n));
-	double y[n];
-	y[0] = 1;
+	double y_next, y_prev = 1.0;
 	
 	std::ofstream write_output("xy.dat");
 	assert(write_output.is_open());
-	write_output << 0 << "," << y[0] << "\n";
+	write_output << 0.0 << "," << y_prev << "\n";
 	
 	for (int i=1; i<n; i++)
 	{
-		y[i] = y[i-1]/(1.0+h);
-		write_output << ((double)(i))*h << "," << y[i] << "\n";
+		y_next = y_prev/(1.0+h);
+		y_prev = y_next;
+		write_output << ((double)(i))*h << "," << y_next << "\n";
 	}
 	write_output.close();
-	
-	return 0;
 }

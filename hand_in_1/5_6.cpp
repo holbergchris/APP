8c571eeaee8b270ffe4817a197c7165779efc0ef
Christian Holberg <holbergchris@hotmail.com>
2019-04-29 13:07:29 +0200

    Exercise 5.6: Final version. Added Multiply functions for matrix*vector, scaler*matrix, and matrix*scalar.

--- hand_in_1/5_6.cpp
+++ hand_in_1/5_6.cpp
@@ -31,3 +31,39 @@ void Multiply(double *res, double *A, double **B, int ACols, int BRows, int BCol
 		res[i] = tmp;
 	}
 }
+
+void Multiply(double *res, double **A, double *B, int ARows, int ACols, int BRows)
+{
+	assert(ACols == BRows);
+	for (int i=0; i<ARows; i++)
+	{
+		double tmp = 0.0;
+		for (int j=0; j<ACols; j++)
+		{
+			tmp += B[j]*A[i][j];
+		}
+		res[i] = tmp;
+	}
+}
+
+void Multiply(double **res, double scalar, double **B, int BRows, int BCols)
+{
+	for (int i=0; i<BRows; i++)
+	{
+		for (int j=0; j<BCols; j++)
+		{
+			res[i][j] = scalar*B[i][j];
+		}
+	}
+}
+
+void Multiply(double **res, double **B, double scalar, int BRows, int BCols)
+{
+	for (int i=0; i<BRows; i++)
+	{
+		for (int j=0; j<BCols; j++)
+		{
+			res[i][j] = scalar*B[i][j];
+		}
+	}
+}
